feb7ab2a6eb013dcfc46bf50c6824ec5ac313a8f
ben17va <114486051+ben17va@users.noreply.github.com>
2023-01-20 19:08:22 +0000

    Add files via upload

--- database_utils.py
+++ database_utils.py
@@ -1,11 +1,11 @@
 import yaml
-import sqlalchemy
-import psycopg2 
+import sqlalchemy 
 from sqlalchemy import inspect
 from sqlalchemy import create_engine
 class DatabaseConnector:
     def __init__(self):
-        self.table_name = 'dim_users'
+        self.database_name = 'Sales_Data'
+        self.table_name = 'dim_product'
 
     def read_db_creds(self):
         with open('db_creds.yaml','r' ) as f:
@@ -13,19 +13,32 @@ class DatabaseConnector:
             return data_loaded
 
 
-    def init_db_engine(self):
+    def init_db_engine(self): 
         creds = self.read_db_creds() 
-        conn_str = 'postgresql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}'.format(**creds)
+        conn_str = conn_str = 'postgresql://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}'.format(**creds)
         engine = sqlalchemy.create_engine(conn_str)
         return engine
 
-    def upload_to_db(self):
-        # import cleaned data
-        from data_cleaning import df_clean
-        # Get the engine
-        engine = self.init_db_engine()
-        # upload data
-        df_clean.to_sql(self.table_name, engine, if_exists='replace')
+
+    def connect_local(self):
+        newconn_str = 'postgresql://{user}:{password}@{host}:{port}/{database}'.format(
+            user='postgres',
+            password='pereira31',
+            host='localhost',
+            port='5432',
+            database='Sales_Data'
+        )
+        engine = create_engine(newconn_str)
+        return engine
+
+    
+    def upload_to_db(self, cleanawsdf, connection_type = 'local'):
+        if connection_type == 'local':
+            engine = self.connect_local()
+        else:
+            engine = self.init_db_engine()()
+    
+        cleanawsdf.to_sql(self.table_name, engine, if_exists='replace',index=False)
 
 
     def list_db_tables(self):
@@ -41,8 +54,7 @@ class DatabaseConnector:
 connector = DatabaseConnector()
 tables = connector.list_db_tables()
 print(tables)
-connector = DatabaseConnector()
-connector.upload_to_db()
+
 
 
 
